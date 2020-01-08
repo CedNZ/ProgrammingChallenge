391a20726e318514957717b22fc5af8392bfae7d
Ced <cbourneville@gmail.com>
2020-01-08 21:22:26 +1300

    Create User model

---
+++ Celo/Celo/Model/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Celo.Model
+{
+    public class User
+    {
+        public int Id { get; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Title { get; set; }
+
+        public string Name => $"{Title}. {FirstName} {LastName}";
+
+        public string Email { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string ProfilePicturePath { get; set; }
+
+        public string ThumbnailPath { get; set; }
+    }
+}
